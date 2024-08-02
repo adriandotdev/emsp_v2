e6b423a776a865078023f0cc3da33b4fc286ae5a
Adrian <marcelonads@gmail.com>
2024-08-02 16:30:00 +0800

    package: ADDED socket.io package

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const app = require("./app");
+const { Server } = require("socket.io");
 
 const httpServer = require("http").createServer(app);
 
+const io = new Server(httpServer);
+
 const logger = require("./config/winston");
+
+io.on("connection", (socket) => {
+	logger.info("client_connected");
+});
 
 httpServer.listen(process.env.PORT, () => {
 	logger.info("Server is running on port: " + process.env.PORT);
